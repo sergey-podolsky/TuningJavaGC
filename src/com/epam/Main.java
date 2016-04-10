5a7d3d980bda786a33eccd85a6fd6a901265472e
Sergey Podolsky <sergey.podolsky@gmail.com>
2016-04-10 14:58:54 +0300

    Add code

---
+++ src/com/epam/Main.java
@@ -0,0 +1,22 @@
+package com.epam;
+
+import java.lang.ref.SoftReference;
+
+public class Main {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        long allocated = 0;
+        SoftReference softReference = null;
+        while (true) {
+            softReference = new SoftReference(softReference);
+            allocated++;
+            long end = System.currentTimeMillis();
+            long elapsed = end - start;
+            if (elapsed > 1000) {
+                System.out.println("Allocated " + allocated + " in " + elapsed + " ms");
+                allocated = 0;
+                start = end;
+            }
+        }
+    }
+}
